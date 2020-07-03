c5b5a2ab90c157b06ce5d6ed1dd73b7dee204290
Edmilson Andrade <edmilisonwork@gmail.com>
2020-07-03 20:28:37 +0100

    defined all the controllers

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -20,5 +20,65 @@ module.exports = {
                 return callBack(null, results)
             }
         )
+    },
+
+    getUsers: callBack => {
+        pool.query(`
+                SELECT id, firstName, lastName, gender, email, number from resgistration`,
+                [],
+                (error, results, fields) => {
+                    if(error){
+                        callBack(error)
+                    }
+                    return callBack(null, results)
+                }
+            )
+    },
+
+    getUsersById: (id, callBack) => {
+        pool.query(`
+                SELECT id, firstName, lastName, gender, email, number from resgistration WHERE id = ?`,
+                [id],
+                (error, results, fields) => {
+                    if(error){
+                        callBack(error)
+                    }
+                    return callBack(null, results[0])
+                }
+            )
+    },
+
+    updateUser: (data, callBack) => {
+        pool.query(
+            `UPDATE registration set firstName=?, lastName=?, gender=?, email=?, password=?, number=? WHERE id = ?)`,
+            [
+                data.first_name,
+                data.last_name,
+                data.gender,
+                data.email,
+                data.password,
+                data.number,
+                data.id
+            ],
+            (error, results, fields) => {
+                if(error){
+                    return callBack(error)
+                }
+                return callBack(null, results[0])
+            }
+        )
+    },
+
+    deleteUser: (data, callBack) => {
+        pool.query(
+            `DELETE from registration WHERE id = ?`,
+            [data.id],
+            (error, results, fields) => {
+                if(error){
+                    return callBack(error)
+                }
+                return callBack(null, results[0])
+            }
+        )
     }
 }
