ad5b30a67b238db5ba3f17bb13bdb41076808039
Edmilson Andrade <edmilisonwork@gmail.com>
2020-07-03 20:33:37 +0100

    defined all routes

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,6 +1,16 @@
-const { createUser } = require("./user.controller")
+const { createUser,
+        getUsers,
+        getUsersById,
+        updateUser,
+        deleteUser
+} = require("./user.controller")
+
 const router = require("express").Router()
 
+router.get('/', getUsers)
+router.get('/:id', getUsersById)
 router.post('/', createUser)
+router.patch('/', updateUser)
+router.delete('/', deleteUser)
 
 module.exports = router
